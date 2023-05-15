486c7096be9b264256e14f684c674c9e850b14a2
Max Gribencicov <max7gri@gmail.com>
2023-05-15 15:27:38 +0300

    Implemented WorkDay service

--- src/aso_lab7/WorkDay.java
+++ src/aso_lab7/WorkDay.java
@@ -1,30 +1,60 @@
 package aso_lab7;
 
-public class WorkDay extends Thread {
-    
-    private int time;
-    
-    public WorkDay() {
-        time = 0;
+import java.util.concurrent.Semaphore;
+
+public class WorkDay {
+    private int chairs;
+    private Semaphore barber = new Semaphore(0);
+    private Semaphore customer = new Semaphore(0);
+    private Semaphore mutex = new Semaphore(1);
+    private boolean sleeping = true;
+    private int currentCustomerId = -1;
+
+    public WorkDay(int chairs) {
+        this.chairs = chairs;
     }
-    
-    public int getTime() {
-        return time;
-    }
-    
-    @Override
-    public void run() {
-        while(true) {
-            try{
-                sleep(1000);
-                time++;
-                if(time>60)
-                    break;
-            }
-            catch(InterruptedException e) {
-                
-            }
+
+    public void enterBarberShop(int customerId) throws InterruptedException {
+        mutex.acquire();
+
+        if (sleeping) {
+            sleeping = false;
+            currentCustomerId = customerId;
+            System.out.println("Customer " + customerId + " wakes up the barber.");
+            barber.release();
+        } else if (chairs > 0) {
+            chairs--;
+            System.out.println("Customer " + customerId + " takes a seat in the waiting room.");
+            customer.release();
+        } else {
+            System.out.println("Customer " + customerId + " leaves because there are no free chairs.");
+            mutex.release();
+            return;
         }
+
+        mutex.release();
+        customer.acquire();
     }
-    
+
+    public void getHaircut(int customerId) throws InterruptedException {
+        System.out.println("Customer " + customerId + " gets a haircut.");
+        Thread.sleep(2000);
+    }
+
+    public void leaveBarberShop(int customerId) throws InterruptedException {
+        mutex.acquire();
+
+        chairs++;
+
+        if (currentCustomerId == customerId) {
+            currentCustomerId = -1;
+            sleeping = true;
+            System.out.println("Customer " + customerId + " wakes up the barber and leaves.");
+            barber.release();
+        } else {
+            System.out.println("Customer " + customerId + " leaves.");
+        }
+
+        mutex.release();
+    }
 }
