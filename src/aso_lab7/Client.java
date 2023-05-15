17f1d98e5bcb04dcebc67d53a937dc1d9201c04f
valio <vali.01@list.ru>
2023-05-15 12:54:40 +0300

    Add client class

--- src/aso_lab7/Client.java
+++ src/aso_lab7/Client.java
@@ -1,16 +1,21 @@
 package aso_lab7;
 
-public class Client extends Thread {
-    
-    private final WorkDay day;
-    
-    public Client(WorkDay _day) {
-        day = _day;
+class Client implements Runnable {
+    private int id;
+    private WorkDay workDay;
+
+    public Client(int id, WorkDay workDay) {
+        this.id = id;
+        this.workDay = workDay;
     }
-    
-    @Override
+
     public void run() {
-        
+        try {
+            workDay.enterBarberShop(id);
+            workDay.getHaircut(id);
+            workDay.leaveBarberShop(id);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
